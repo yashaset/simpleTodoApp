8c330fb3606b48a5d182fbdaf40b799ed3b6d798
Yash Rawat <39219898+yashaset@users.noreply.github.com>
2021-06-08 17:02:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+// event Listener
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
+// functions
+function addTodo(e) {
+  e.preventDefault();
+  // console.log(e);
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // create li
+  const newTodo = document.createElement('li');
+
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  // trash mark button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+  // append to list
+  todoList.appendChild(todoDiv);
+  // clear input value
+  todoInput.value = '';
+}
+function deleteCheck(e) {
+  // console.log(e.target);
+  const item = e.target;
+  // delete todo
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  // console.log(todos);
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  });
+}
